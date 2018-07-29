7799738bb55076488e93a8e365104ae7a1ffb140
Parth Nandaniya <parthnandaniya@gmail.com>
2018-07-29 20:40:11 +0530

    change contact

--- src/screens/settingScreen.js
+++ src/screens/settingScreen.js
@@ -65,8 +65,7 @@ class settingScreen extends Component {
 					</View>
 					<View style={styles.viewComponent} >
 						<Text style={[styles.textHeaderStyle, {color: this.props.color}]} > contact </Text>
-						<Text style={styles.textStyle} > twitter.com/Parth_Nandaniya </Text>
-						<Text style={styles.textStyle} > github.com/ParthNandaniya </Text>
+						<Text style={styles.textStyle} > parthnandaniya@gmail.com </Text>
 					</View>
 				</View>
 			</View>
